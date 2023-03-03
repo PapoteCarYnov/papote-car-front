97b134c71a913536c358b0188f7cda09747220e9
kana03 <camille.naulet@hotmail.fr>
2023-03-03 15:57:37 +0100

    add 404 and routes restriction

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
 import SearchView from "@/views/SearchView.vue";
+import NotFound from "@/views/NotFoundView.vue";
 
 const routes = [
   {
@@ -18,8 +19,15 @@ const routes = [
     path: '/search',
     name: 'search',
     component: SearchView
-  }
+  },
+  { path: '/404', component: NotFound },
+  { path: '/:catchAll(.*)', redirect: '/404' }
 ];
+
+// function beforeEnter(to, from, next) {
+//   if (localStorage.getItem("user-token")) next();
+//   else next('/');
+// }
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="notfound">
+    <h1>Erreur 404</h1>
+    <v-img :src="require('../assets/404.svg')" width="700px" id="error" alt="error404"></v-img>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NotFoundView'
+}
+
+</script>
+<style scoped>
+#notfound {
+  color: #F58926;
+  text-align: center;
+  padding-top: 5%;
+}
+
+#error {
+  display: inline-block;
+}
+</style>
